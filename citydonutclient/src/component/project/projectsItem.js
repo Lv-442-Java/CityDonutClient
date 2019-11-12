ad0937393f2ac5e040698261eee99da0a66f3621
Nazar Hladysh <nazar.gladish@gmail.com>
2019-11-12 15:22:53 +0200

    add projectItem and projectList

--- citydonutclient/src/component/project/projectsItem.js
+++ citydonutclient/src/component/project/projectsItem.js
@@ -1,26 +1,55 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import ProgressBar from "../progressBar/progressBar";
 
-export class ProjectsItem extends React.Component{
+export class ProjectsItem extends React.Component {
+
+    state = {
+        donatesSum: 0,
+        donatedPercent: 0,
+        arr: this.props.categories.map(category => category.category)
+    };
+
+    getDonatesSum = () => {
+        axios.get(`http://localhost:8091/api/v1/donates/all/projects/${this.props.id}`, {crossDomain: true}).then(response => {
+            this.setState({
+                donatesSum: response.data,
+                donatedPercent: response.data * 100 / this.props.moneyNeeded
+            })
+        })
+    };
+
+    componentDidMount() {
+        this.getDonatesSum();
+    }
 
     render() {
         return (
             <div>
-                {/*{this.props.name}*/}
-                <Card border="primary" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="https://i0.wp.com/storage.googleapis.com/blog-images-backup/1*3SVfBkNZI2f-sspiq59xcw.png?zoom=1.25&resize=391%2C321&ssl=1" />
-                    <Card.Body>
-                        <Card.Title>{this.props.name}</Card.Title>
-                        <Card.Text>
-                            Категорія: {this.props.categories.map(category => category.category + ", ")}
-                        </Card.Text>
-                        <Card.Text>
-                            Статус: {this.props.status.status}
-                        </Card.Text>
-                        {/*<Button variant="primary" href={'projects/' + this.props.id}>Go somewhere</Button>*/}
-                    </Card.Body>
-                </Card>
+                <Link to={`projects/${this.props.id}`} style={{textDecoration: 'none', color: 'black'}}>
+                    <Card border="primary" className="text-center mx-auto"
+                          style={{width: '18rem', marginTop: '2rem'}}>
+                        <Card.Img variant="top"
+                                  src="https://i0.wp.com/storage.googleapis.com/blog-images-backup/1*3SVfBkNZI2f-sspiq59xcw.png?zoom=1.25&resize=391%2C321&ssl=1"/>
+                        <Card.Body>
+                            <Card.Title>{this.props.name}</Card.Title>
+
+                            <Card.Text style={{height: '3rem'}}>
+                                Категорії: {this.state.arr.join(", ")}
+                            </Card.Text>
+
+                            <Card.Text>
+                                Статус: {this.props.status.status}
+                            </Card.Text>
+
+                            <ProgressBar doneTip="Зібрано!" donePercent={this.state.donatedPercent}
+                                         doneLabel={this.state.donatesSum} undoneTip="Потрібно зібрати!"
+                                         undoneLabel={this.props.moneyNeeded - this.state.donatesSum}/>
+                        </Card.Body>
+                    </Card>
+                </Link>
             </div>
         )
     }
--- citydonutclient/src/component/project/projectsList.js
+++ citydonutclient/src/component/project/projectsList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {ProjectsItem} from "./projectsItem";
-import {Col, Container, Row} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import CardDeck from "react-bootstrap/CardDeck";
 
 export class ProjectsList extends React.Component {
 
     render() {
         return (
-
             <div>
                 <Container>
-                    <CardDeck bsPrefix="card-deck d-flex justify-content-around">
+                    <CardDeck className="d-flex justify-content-around">
                         {this.props.projects.map(element =>
-                                <ProjectsItem key={element.id} id={element.id} name={element.name} categories={element.categories} status={element.projectStatusDto}/>
+                            <ProjectsItem key={element.id} id={element.id} name={element.name}
+                                          categories={element.categories} status={element.projectStatusDto}
+                                          moneyNeeded={element.moneyNeeded} />
                         )}
                     </CardDeck>
                 </Container>
