4f6395a6877a354135b88b47e2fde49516f092c6
martavoloshyn <martavoloshyn73@gmail.com>
2019-11-15 17:14:17 +0200

    change if

--- citydonutclient/src/component/project/projects.js
+++ citydonutclient/src/component/project/projects.js
@@ -34,18 +34,10 @@ export class Projects extends React.Component {
         let url = `?` +
             `page=${this.state.filters.page}&` +
             `size=${this.state.filters.size}`;
-        if (this.state.filters.status !== null) {
-            url += `&status=${this.state.filters.status}`;
-        }
-        if (this.state.filters.moneyFrom !== null) {
-            url += `&moneyFrom=${this.state.filters.moneyFrom}`
-        }
-        if (this.state.filters.moneyTo !== null) {
-            url += `&moneyTo=${this.state.filters.moneyTo}`;
-        }
-        if (this.state.filters.categories.length !== 0) {
-            url += `&categories=${this.state.filters.categories}`
-        }
+        this.state.filters.status !== null && (url += `&status=${this.state.filters.status}`);
+        this.state.filters.moneyFrom !== null && (url += `&moneyFrom=${this.state.filters.moneyFrom}`);
+        this.state.filters.moneyTo !== null && (url += `&moneyTo=${this.state.filters.moneyTo}`);
+        this.state.filters.categories.length !== 0 && (url += `&categories=${this.state.filters.categories}`);
         this.props.history.push(url);
         return url;
     };
