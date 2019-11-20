454f908764216ea39c3c1245e47a475b122b9bc0
nastiamazurak <nastia.mazurak@gmail.com>
2019-11-20 16:39:42 +0200

    added projectScroller

--- citydonutclient/src/component/project/project.js
+++ citydonutclient/src/component/project/project.js
@@ -1,44 +1,53 @@
-import React from "react";
-import {PhotoSlider} from "./photoSlider";
-import {ProjectProgressBar} from "./projectProgressBar";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
+import { PhotoSlider } from './photoSlider';
+import { ProjectProgressBar } from './projectProgressBar';
+import {ProjectScroller} from "./projectScroller";
 
-export class Project extends React.Component{
+export class Project extends React.Component {
     state= {
         project: {},
+        // eslint-disable-next-line react/prop-types
         projectId: this.props.match.params.id,
     }
 
     componentDidMount() {
-        this.getData()
+        this.getData();
     }
 
+    // eslint-disable-next-line react/sort-comp
     getData = () => {
-        axios.get(`http://localhost:8091/api/v1/project/${this.state.projectId}`, { withCredentials: true }).then(response => {
-        this.setState({project: response.data})
-    })
-
+        axios.get(`http://localhost:8091/api/v1/project/${this.state.projectId}`, { withCredentials: true }).then((response) => {
+            this.setState({ project: response.data });
+        });
     };
 
     componentDidUpdate(prevProps) {
-
+        // eslint-disable-next-line react/prop-types
         if (this.props.moneyNeeded !== prevProps.moneyNeeded) {
+            // eslint-disable-next-line react/prop-types
             this.fetchData(this.props.moneyNeeded);
         }
     }
-    render() {
 
-        return(
+    render() {
+        return (
 
             <div>
-                {(this.state.project.moneyNeeded != null ) ? (
+                {(this.state.project.moneyNeeded != null) ? (
                     <div>
-                        <PhotoSlider projectId={this.state.projectId} projectName={this.state.project.name}></PhotoSlider>
-                        <ProjectProgressBar projectId={this.state.projectId} projectName={this.state.project.name}
-                        moneyNeeded={this.state.project.moneyNeeded}
-                        endDate={this.state.project.donationEndDate}></ProjectProgressBar>
+                        {/* eslint-disable-next-line max-len */}
+                        <PhotoSlider projectId={this.state.projectId} projectName={this.state.project.name} />
+                        <ProjectProgressBar
+                            projectId={this.state.projectId}
+                            projectName={this.state.project.name}
+                            moneyNeeded={this.state.project.moneyNeeded}
+                            endDate={this.state.project.donationEndDate}
+                        />
+                        <ProjectScroller projectId={this.state.projectId}
+                                         description={this.state.project.description}></ProjectScroller>
                     </div>
-                    ): (<h1>Something went wrong. Reload the page, please</h1>)}
+                ) : (<h1>Something went wrong. Reload the page, please</h1>)}
             </div>
         );
     }
---
+++ citydonutclient/src/component/project/projectScroller.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import {Description} from "./description";
+import ScrollUpButton from "react-scroll-up-button";
+import {Document} from "./documentation"
+import {ProjectDonates} from "./projectDonates";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+export class ProjectScroller extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <Container>
+                    <div className="d-flex justify-content-center " style={{width:"100%", height:"35px"}}>
+                        <div className= "d-flex justify-content-around align-items-center" style = {{background: "#f0f0f0", margin: "20px",
+                            width:"60%", height:"35px"}}>
+                            <Link
+                                activeClass="active"
+                                to="#description"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}>
+                                Опис
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="docs"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}>
+                                Документація
+                            </Link>
+                            <Link
+
+                                activeClass="active"
+                                to="map"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}>
+                                Розташування
+                            </Link>
+                            <Link
+
+                                activeClass="active"
+                                to="donates"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}>
+                                Внески
+                            </Link>
+                            {this.props.status === "реалізація" || this.props.status === "виконаний" && <Link
+                                activeClass="active"
+                                to="map"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}>
+                                Сторіборд
+                            </Link>}
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="" style={{width:"80%", margin:"30px"}} id= "description">
+                        <h3>Опис</h3>
+                        <Description description={this.props.description}></Description>
+                    </div>
+                    <br/>
+                    <div className="" style={{width:"80%", margin: "30px"}} id = "docs">
+                        <h3>Документація</h3>
+                        <Document projectId = {this.props.projectId}></Document>
+                    </div>
+                    <br/>
+                    <div className="" style={{width:"80%", margin: "30px"}} id = "map">
+                        <h3>Розташування</h3>
+                        <p>map map map</p>
+                    </div>
+
+                    <br/>
+                    <div  style={{width:"80%", margin: "30px"}} id = "donates">
+                        <h3>Внески</h3>
+                        <br/>
+                        <div className="">
+                            <ProjectDonates projectId={this.props.projectId}></ProjectDonates>
+                        </div>
+                    </div>
+
+                </Container>
+                <div>
+                    <ScrollUpButton />
+                </div>
+            </div>
+        );
+    }
+}
+
+
