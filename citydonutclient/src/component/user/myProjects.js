2d03c8b09e41e643b6060bf92dee8768195aaa83
nastiamazurak <nastia.mazurak@gmail.com>
2019-12-07 14:22:46 +0200

    added filters to userProjects

--- citydonutclient/src/component/user/myProjects.js
+++ citydonutclient/src/component/user/myProjects.js
@@ -2,18 +2,28 @@ import React from 'react';
 import axios from 'axios';
 import jwt from 'jwt-decode';
 import { ProjectsList } from '../project/projectsList';
+import {ProjectsFilter} from "../project/projectsFilter";
 
 export class MyProjects extends React.Component {
+
     state ={
         projects: [],
         id: undefined,
         url: undefined,
+        page: 0,
+        newProjects: [],
+        filters: {}
 
     };
 
     componentDidMount() {
         this.getRole();
     }
+
+    setFilters = (filters) => {
+        this.setState({ filters },
+            () => this.getData());
+    };
 
     cookiesToJson = () => Object.fromEntries(document.cookie.split(/; */).map((c) => {
         const [key, ...v] = c.split('=');
@@ -40,9 +50,35 @@ export class MyProjects extends React.Component {
             });
     };
 
+    showMoreItems = () => {
+        this.setState({ page: this.state.page + 1 }, () => {
+            axios.get(`http://localhost:8091/api/v1/project/filter${this.state.url}=${this.state.id}&page=${this.state.page}`,
+                { withCredentials: true })
+                .then((response) => {
+                    this.setState({
+                        projects: this.state.projects.concat(response.data),
+                        newProjects: response.data,
+                    });
+                });
+        });
+    };
+
     render() {
         return (
-            <ProjectsList projects={this.state.projects} />
+            <div className="row">
+                <div className="col-md-3 col-sm-3 col-lg-3 col-xs-12">
+                    <ProjectsFilter
+                        isOwner={true}
+                        setFilters={this.setFilters}
+                        startLink={this.props.location.search}
+                    />
+                </div>
+                <div className="col-md-9 col-sm-9 col-lg-9 col-xs-12">
+            <ProjectsList projects={this.state.projects}
+                          showMore={this.showMoreItems}
+                          newProjects={this.state.newProjects}/>
+                </div>
+            </div>
         );
     }
 }
