c04626a2d60362577b2012b44c8e8d3d95766dfc
NazarRyvak <ryvaknt96@gmail.com>
2019-12-11 15:42:23 +0200

    footer

--- citydonutclient/src/routers.js
+++ citydonutclient/src/routers.js
@@ -1,41 +1,45 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import {Route, Switch} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 import Login from './component/login/login';
-import { Base } from './component/base/base';
-import { Head } from './component/head/head';
-import { CreateProject } from './component/project/createNewProject/CreateProject';
-import { Projects } from './component/project/projects';
+import {Base} from './component/base/base';
+import {Head} from './component/head/head';
+import {CreateProject} from './component/project/createNewProject/CreateProject';
+import {Projects} from './component/project/projects';
 import UpdateProject from './component/project/updateAxistProject/UpdateProject';
-import { Project } from './component/project/project';
-import { Registration } from './component/auth/registration';
-import { ActivationUser } from './component/auth/activationUser';
-import { UserEdit } from './component/user/user_edit';
+import {Project} from './component/project/project';
+import {Registration} from './component/auth/registration';
+import {ActivationUser} from './component/auth/activationUser';
+import {UserEdit} from './component/user/user_edit';
 import DonatedProjects from './component/project/donatedProjects';
-import { UpdateProjectPage } from './component/project/updateProject/UpdateProjectPage';
-import { MyProjects } from './component/user/myProjects';
+import {UpdateProjectPage} from './component/project/updateProject/UpdateProjectPage';
+import {MyProjects} from './component/user/myProjects';
 import FooterPage from './component/base/footer';
 
 export class Routers extends React.Component {
     render() {
         return (
-            <BrowserRouter  >
-                <Head />
-                <Switch >
-                    <Route path="/project/update" component={UpdateProject} />
-                    <Route exact path="/" component={Base} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/project/create" component={CreateProject} />
-                    <Route path="/project/updatepage" component={UpdateProjectPage} />
-                    <Route path="/registration" component={Registration} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route path="/projects/:id" render={props => <Project {...props} id={this.props.id} />} />
-                    <Route path="/activationUser/:activationCode" component={ActivationUser} />
-                    <Route path="/donates/projects" component={DonatedProjects} />
-                    <Route path="/user" component={UserEdit} />
-                    <Route path="/userprojects" component={MyProjects} />
-                </Switch>
-                <FooterPage />
+            <BrowserRouter>
+                <div style={{minHeight: '100vh'}}>
+                    <Head/>
+                    <Switch>
+                        <Route path="/project/update" component={UpdateProject}/>
+                        <Route exact path="/" component={Base}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/project/create" component={CreateProject}/>
+                        <Route path="/project/updatepage" component={UpdateProjectPage}/>
+                        <Route path="/registration" component={Registration}/>
+                        <Route exact path="/projects" component={Projects}/>
+                        <Route path="/projects/:id" render={props => <Project {...props} id={this.props.id}/>}/>
+                        <Route path="/activationUser/:activationCode" component={ActivationUser}/>
+                        <Route path="/donates/projects" component={DonatedProjects}/>
+                        <Route path="/user" component={UserEdit}/>
+                        <Route path="/userprojects" component={MyProjects}/>
+                    </Switch>
+                </div>
+                <div id='footer'>
+                    <FooterPage/>
+                </div>
             </BrowserRouter>
         );
     }
